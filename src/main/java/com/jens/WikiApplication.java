b1333f65d20b671632a93cde9f24f982ffed2a75
young <2314486494@qq.com>
2023-10-23 19:38:46 +0800

    修改启动文案

--- src/main/java/com/jens/WikiApplication.java
+++ src/main/java/com/jens/WikiApplication.java
@@ -1,13 +1,21 @@
 package com.jens;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class WikiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        ApplicationContext app = SpringApplication.run(WikiApplication.class, args);
+        Environment env = app.getEnvironment();
+        log.info("启动成功");
+        log.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
