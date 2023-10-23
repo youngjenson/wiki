08cf42ff33a3c3539b268503cb36d7afa13302ca
young <2314486494@qq.com>
2023-10-23 19:52:42 +0800

    添加启动图案

--- src/main/java/com/jens/WikiApplication.java
+++ src/main/java/com/jens/WikiApplication.java
@@ -1,7 +1,6 @@
 package com.jens;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
